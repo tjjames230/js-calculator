2e6d5403ae8af7118f3ad62e5e2adb39316e6d1e
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2023-09-20 15:39:18 -0500

    removed unnecessary variables

--- app.js
+++ app.js
@@ -1,26 +1,6 @@
+/*
 const zero = Number(document.querySelector("#zero").innerHTML);
-const one = Number(document.querySelector("#one").innerHTML);
-const two = Number(document.querySelector("#two").innerHTML);
-const three = Number(document.querySelector("#three").innerHTML);
-const four = Number(document.querySelector("#four").innerHTML);
-const five = Number(document.querySelector("#five").innerHTML);
-const six = Number(document.querySelector("#six").innerHTML);
-const seven = Number(document.querySelector("#seven").innerHTML);
-const eight = Number(document.querySelector("#eight").innerHTML);
-const nine = Number(document.querySelector("#nine").innerHTML);
-
-const numbers = {
-  zero: 0,
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9,
-};
+*/
 
 const operators = {
   "+": function (a, b) {
